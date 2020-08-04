c9f8b0295597993f4f1452373f70680924c48638
Lewis Nyman <hello@lewisnyman.co.uk>
2020-08-04 16:10:47 +0100

    Update gulpfile for compatibility with v4

--- gulpfile.js
+++ gulpfile.js
@@ -20,14 +20,14 @@ var buildDir = './destination'
 var sassDir = './source/_sass/**/*.scss';
 var cssDir = './destination/css';
 var jsDir = './source/js/*';
-var jsSrc = ['./source/js/*.js','./source/js/track-outbound-links.js'];
+var jsSrc = ['./source/js/*.js', './source/js/track-outbound-links.js'];
 var jsDest = './destination/js';
 var imgSrc = './source/images/**/*.{jpg,jpeg,png}';
 var svgSrc = './source/images/**/*.svg';
 var imgDest = './destination/images';
 
 gulp.task('clean', function () {
-  return gulp.src(buildDir, {read: false})
+  return gulp.src(buildDir, { read: false })
     .pipe(clean());
 });
 
@@ -35,97 +35,97 @@ gulp.task('sass', function () {
   return gulp.src(sassDir)
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false,
-        grid: true
+      browsers: ['last 2 versions'],
+      cascade: false,
+      grid: true
     }))
     .pipe(cleanCSS())
     .pipe(gulp.dest(cssDir))
     .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   gulp.src(jsSrc)
     .pipe(browserify({
-      insertGlobals : true,
-      debug : !gulp.env.production
+      insertGlobals: true,
+      debug: !gulp.env.production
     }))
     .pipe(uglify())
     .pipe(gulp.dest(jsDest));
-    browserSync.reload();
-});
-
-gulp.task('svgs', function() {
-  gulp.src(svgSrc)
-  .pipe(imagemin())
-  .pipe(gulp.dest(imgDest))
   browserSync.reload();
 });
 
-gulp.task('bitmaps', function() {
+gulp.task('svgs', function () {
+  gulp.src(svgSrc)
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDest))
+  browserSync.reload();
+});
+
+gulp.task('bitmaps', function () {
   gulp.src(imgSrc)
-  .pipe(responsive({
-    '**/*': [
-      {
-        width: 500,
-        rename: { suffix: '--medium' },
-      },
-      {
-        width: 500 * 2,
-        rename: { suffix: '--medium@2x' },
-      },
-      {
-        width: 250,
-        rename: { suffix: '--small' },
-      },
-      {
-        width: 250 * 2,
-        rename: { suffix: '--small@2x' },
-      }
-    ],
-  }, {
-    // Global configuration for all images
-    // The output quality for JPEG, WebP and TIFF output formats
-    quality: 70,
-    strictMatchImages: false,
-    // Use progressive (interlace) scan for JPEG and PNG output
-    progressive: true,
-    // Zlib compression level of PNG output format
-    compressionLevel: 6,
-    // Strip all metadata
-    withMetadata: false,
-    withoutEnlargement: false,
-  }))
-  .pipe(gulp.dest(imgDest))
+    .pipe(responsive({
+      '**/*': [
+        {
+          width: 500,
+          rename: { suffix: '--medium' },
+        },
+        {
+          width: 500 * 2,
+          rename: { suffix: '--medium@2x' },
+        },
+        {
+          width: 250,
+          rename: { suffix: '--small' },
+        },
+        {
+          width: 250 * 2,
+          rename: { suffix: '--small@2x' },
+        }
+      ],
+    }, {
+      // Global configuration for all images
+      // The output quality for JPEG, WebP and TIFF output formats
+      quality: 70,
+      strictMatchImages: false,
+      // Use progressive (interlace) scan for JPEG and PNG output
+      progressive: true,
+      // Zlib compression level of PNG output format
+      compressionLevel: 6,
+      // Strip all metadata
+      withMetadata: false,
+      withoutEnlargement: false,
+    }))
+    .pipe(gulp.dest(imgDest))
   browserSync.reload();
 
 });
 
 gulp.task('critical', ['generate-site'], function (cb) {
   return gulp.src('destination/**/*.html')
-  .pipe(critical({
+    .pipe(critical({
       base: 'destination/',
       css: ['destination/css/style.css'],
       inline: true,
       dimensions: [{
         width: 320,
         height: 480
-      },{
+      }, {
         width: 768,
         height: 1024
       }]
-  }))
-  .on('error', function(err) { console.log(err.message); })
-  .pipe(gulp.dest('destination'));
+    }))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(gulp.dest('destination'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp
     // Run run `sass` task when sass files change
     .watch(sassDir, ['sass'])
     // When there is a change,
     // log a message in the console
-    .on('change', function(event) {
+    .on('change', function (event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
@@ -134,7 +134,7 @@ gulp.task('watch', function() {
     .watch(jsDir, ['scripts'])
     // When there is a change,
     // log a message in the console
-    .on('change', function(event) {
+    .on('change', function (event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
@@ -143,7 +143,7 @@ gulp.task('watch', function() {
     .watch(imgSrc, ['bitmaps'])
     // When there is a change,
     // log a message in the console
-    .on('change', function(event) {
+    .on('change', function (event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
@@ -152,7 +152,7 @@ gulp.task('watch', function() {
     .watch(svgSrc, ['svgs'])
     // When there is a change,
     // log a message in the console
-    .on('change', function(event) {
+    .on('change', function (event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 
@@ -172,16 +172,16 @@ gulp.task('serve', function () {
   });
 });
 
-gulp.task('deploy', ['critical'], function() {
+gulp.task('deploy', gulp.series('critical', function () {
   return gulp.src('./destination/**/*')
     .pipe(ghPages());
-});
+}));
 
 gulp.task('jekyll-build', shell.task('bundle exec jekyll build --config jekyllconfig.yml'));
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function () {
   browserSync.reload();
-});
+}));
 
-gulp.task('generate-site', ['jekyll-build', 'sass', 'scripts', 'bitmaps', 'svgs'])
-gulp.task('build', ['critical', 'generate-site']);
-gulp.task('default', ['generate-site', 'serve', 'watch']);
+gulp.task('generate-site', gulp.parallel('jekyll-build', 'sass', 'scripts', 'bitmaps', 'svgs'))
+gulp.task('build', gulp.parallel('critical', 'generate-site'));
+gulp.task('default', gulp.parallel('generate-site', 'serve', 'watch'));
