dd6c884c376a53035394810858b198714f4fcbf7
Lewis Nyman <hello@lewisnyman.co.uk>
2020-10-21 13:32:27 +0100

    Initial re-implementation of team listing

--- .eleventy.js
+++ .eleventy.js
@@ -37,6 +37,13 @@ module.exports = function (eleventyConfig) {
     strict_filters: true,
   });
 
+  // Content collections
+  // Sort team members by 'order' field
+  eleventyConfig.addCollection('team', (collection) => {
+    var team = collection.getFilteredByTag('team');
+    return utils.sortByOrder(team);
+  });
+
   // Responsive images
   eleventyConfig.addPlugin(pluginLocalRespimg, {
     folders: {
@@ -86,4 +93,14 @@ module.exports = function (eleventyConfig) {
       output: './destination',
     },
   };
+};
+
+const utils = {
+  sortByOrder: function (collection) {
+    return collection.sort((a, b) => {
+      if (a.data.order < b.data.order) return -1;
+      else if (a.data.order > b.data.order) return 1;
+      else return 0;
+    });
+  },
 };
---
+++ source/team/nuria-ruiz.md
@@ -0,0 +1,12 @@
+---
+first_name: Núria
+last_name: Ruiz
+job: User Research
+location: Edinburgh
+readmore_link: 'https://blog.weareconvivio.com/meet-the-convivio-team-n%C3%BAria-ruiz-e6b3e7725ded'
+dyk: 'There is no limit to the number of shopping bags Núria can carry up the stairs.'
+order: 0
+image: '/_assets/images/team/nuria-ruiz.jpg'
+---
+
+I carry out research with people who use digital services, exploring where we can make things better. You'll find me running interviews, workshops and tests, sharing what I learn back with the team.
